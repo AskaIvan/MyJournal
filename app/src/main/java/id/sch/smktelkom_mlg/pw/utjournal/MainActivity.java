aa9f20751af834a3800ebd1e68e6d3debb3c18e6
nurwid28 <nur_astutik_24rpl@student.smktelkom-mlg.sch.id>
2018-01-05 10:39:50 +0700

    SignUp

--- app/src/main/java/id/sch/smktelkom_mlg/pw/utjournal/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/pw/utjournal/MainActivity.java
@@ -8,8 +8,29 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import id.sch.smktelkom_mlg.pw.utjournal.Model.User;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    //firebase db
+    FirebaseDatabase database;
+    DatabaseReference users1;
+
+    EditText edtusername,edtpassword, edtemail, edtbranch, edtarea, edtnrp;
+    Button btnSignUp;
 
     AppBarLayout Appbar;
     CollapsingToolbarLayout collTolbar;
@@ -20,20 +41,60 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        //firebase
+        database = FirebaseDatabase.getInstance();
+        users1 = database.getReference("Users1");
+
+        edtusername = (EditText) findViewById(R.id.edtusername);
+        edtpassword = (EditText) findViewById(R.id.edtpassword);
+        edtemail = (EditText) findViewById(R.id.edtemail);
+        edtbranch = (EditText) findViewById(R.id.edtbranch);
+        edtarea= (EditText) findViewById(R.id.edtarea);
+        edtnrp = (EditText) findViewById(R.id.edtnrp);
+
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final User user = new User(edtusername.getText().toString(),
+                        edtpassword.getText().toString(),
+                        edtemail.getText().toString(),
+                        edtbranch.getText().toString(),
+                        edtarea.getText().toString(),
+                        edtnrp.getText().toString());
+
+                users1.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.child(user.getUsername()).exists())
+                            Toast.makeText(MainActivity.this, "Username Sudah Ada!", Toast.LENGTH_SHORT).show();
+                        else {
+                            users1.child(user.getUsername()).setValue(user);
+                            Toast.makeText(MainActivity.this, "Registrasi Sukses!!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        //custom code
+                    }
+                });
+            }
+        });
 
 
 
 
-
-
-        FloatingActionButton fab = findViewById(R.id.fab);
+        /*FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 }
---
+++ app/src/main/java/id/sch/smktelkom_mlg/pw/utjournal/Model/User.java
@@ -0,0 +1,77 @@
+package id.sch.smktelkom_mlg.pw.utjournal.Model;
+
+/**
+ * Created by Widya on 1/4/2018.
+ */
+
+public class User {
+
+    private String username;
+    private String password;
+    private String email;
+    private String branch;
+    private String area;
+    private String nrp;
+
+    public User(){
+
+    }
+
+    public User( String username, String password, String email, String branch, String area, String nrp){
+        this.username = username;
+        this.password = password ;
+        this.email = email;
+        this.branch = branch;
+        this.area = area;
+        this.nrp = nrp;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getBranch(){
+        return branch;
+    }
+
+    public void setBranch(String branch){
+        this.branch = branch;
+    }
+
+    public String getArea(){
+        return area;
+    }
+
+    public void setArea(String area){
+        this.area = area;
+    }
+
+    public String getNrp(){
+        return nrp;
+    }
+
+    public void setNrp(String nrp){
+        this.nrp = nrp;
+    }
+
+}
